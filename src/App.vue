bb92f48011e8540a6088eb4b0caaef285ea21cbc
wellheor1 <Well.heor@yandex.ru>
2024-02-22 09:37:51 +0800

    create proj

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="full-page">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.full-page {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+</style>
